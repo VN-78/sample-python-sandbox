109c3d26da94ffad27cb95425970660c4ac53135
VN-78 <vishwanath.s.god@gmail.com>
2025-10-30 01:48:11 +0530

    Second Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello from sample-python-sandbox!")
+    
+    print ("Sample output to demonstrate the sandbox environment.")
+    
+    a = 5 ;
+    b = 10 ;
+    sum = a + b ;
+    print("The sum of", a, "and", b, "is", sum)
+
+
+if __name__ == "__main__":
+    main()
